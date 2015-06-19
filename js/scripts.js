1922c2be48ae0c7c795bff0019dce03d93971a0e
Daniel <d.galati54@gmail.com>
2015-06-18 21:42:25 -0500

    Revised logic for select players, implemented reset button, partially implemented submit button

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,13 @@
  */
 
 $(function() {
-    var allPlayers = []; //get request here for player ids
-    allPlayers.push("Daniel", "Steve", "Matt", "Adriano");
 
+    var allPlayers = ["Daniel", "Steve", "Matt", "Adriano"]; //get request here for player ids
     var availablePlayers = ["Daniel", "Steve", "Matt", "Adriano"];
-
+    var rootURL = "http://firestone.gyges.feralhosting.com/foosball/";
 
     var red1 = $('#red1');
     var red2 = $('#red2');
-
     var black1 = $('#black1');
     var black2 = $('#black2');
 
@@ -19,6 +17,13 @@ $(function() {
 
     function resetPlayers() {
         (function() {//resets player input boxes
+            var allPlayers = ["Daniel", "Steve", "Matt", "Adriano"];
+            var availablePlayers = ["Daniel", "Steve", "Matt", "Adriano"];
+
+            for (var p=0; p < optionBoxes.length; p++) {
+                optionBoxes[p].find('option').remove();
+            }
+
             for (var j=0; j < optionBoxes.length; j++)
             {
                 optionBoxes[j].append($('<option></option>').val('').html(''));
@@ -26,6 +31,7 @@ $(function() {
                     optionBoxes[j].append($('<option></option>').val(allPlayers[k]).html(allPlayers[k]));
                 }
             }
+
         })()
     }
 
@@ -41,6 +47,30 @@ $(function() {
         })();
     }
 
+    function getRedPlayers() {
+        var redPlayers = [];
+        for (var j = 0; j < optionBoxes.length; j++) {
+            optBoxStr = optionBoxes[j].selector
+
+            if ((optBoxStr.indexOf('red') === 1) && (optionBoxes[j].val().length > 0)) {
+                redPlayers.push(optionBoxes[j].val());
+            }
+        }
+        return redPlayers;
+    }
+
+    function getBlackPlayers() {
+        var blackPlayers = [];
+        for (var j = 0; j < optionBoxes.length; j++) {
+            optBoxStr = optionBoxes[j].selector
+
+            if ((optBoxStr.indexOf('black') === 1) && (optionBoxes[j].val().length > 0)) {
+                blackPlayers.push(optionBoxes[j].val());
+            }
+        }
+        return blackPlayers;
+    }
+
 
 
 
@@ -48,25 +78,62 @@ $(function() {
     $('.score').find('input').val(0);
     $(".players").change(function(e) {
         console.log(e.target.value);
+        //debugger;
+        //taking person out of available players array
         var selectedPlayer = e.target.value;
         for (var j=0; j <availablePlayers.length; j++) {
             if (selectedPlayer === availablePlayers[j]) {
                 availablePlayers.splice([j], 1);
+                break;
             }
         }
+        //taking person out of options box
         for (var j=0; j < optionBoxes.length; j++) {
-            debugger;
             if (optionBoxes[j][0] !== e.target) {
                 //alert('t');
                 optionBoxes[j].find('option[value=' + selectedPlayer + ']').remove();
             }
         }
 
+        //if current target == adriano, steve, matt, daniel
+        //get person, add them back to others
+
+        //section to add back players to list
+
+
+
 
     });
 
     $('#reset').click(function() {
-        reset();
+        resetPlayers();
+        resetScore();
+
+    })
+
+    $('#submit').click(function() {
+        debugger;
+        var blackPlayers = getBlackPlayers();
+        var redPlayers = getRedPlayers();
+        var redTeamPresent = redPlayers.length > 0;
+        var blackTeamPresent = blackPlayers.length > 0;
+        var blackScore = $('#blackScore').val();
+        var redScore = $('#redScore').val();
+        var rootURL = "http://firestone.gyges.feralhosting.com/foosball/";
+
+        var gameDetailsURL = 'game?side1=';
+
+
+        var failMsg = "Submit Failed."
+        if ((!redTeamPresent) || !(blackTeamPresent)) {
+            alert(failMsg + '\nEach team must have atleast one player!');
+        }
+
+
+
+
+
+
 
     })
 
